2922ad90da769563bccff4dd59dc0fb9850f9c2b
RamilSundukov <syndykov.com@gmail.com>
2022-02-16 10:36:16 +0500

    14.02.22

--- site/cycle.php
+++ site/cycle.php
@@ -18,9 +18,12 @@
             1 задание
         </legend>
             <?
-                for ($i = 5; $i <= 13; $i++){
-                    echo '<br>'. $i;
+                function f1()
+                {
+                    for ($i = 5; $i <= 13; $i++)
+                        echo '<br>' . $i;
                 }
+                f1();
             ?>
     </fieldset>
     <fieldset>
@@ -29,12 +32,16 @@
         </legend>
             <?
                 $num = 1000;
-                $count = 0;
-                while ($num >= 50){
-                    $num = $num / 2;
-                    $count++;
+                function f2w($num)
+                {
+                    $count = 0;
+                    while ($num >= 50) {
+                        $num = $num / 2;
+                        $count++;
+                    }
+                    echo 'Получится число:' . $num . '<br>Количество итераций:' . $count;
                 }
-                echo 'Получится число:'. $num .'<br>Количество итераций:'. $count;
+                f2w($num);
             ?>
     </fieldset>
     <fieldset>
@@ -43,12 +50,16 @@
         </legend>
             <?
                 $num = 0;
-                $count = 0;
-                for ($i = 1000; $i >= 50; $i = $i / 2){
-                    $num = $i / 2;
-                    $count++;
+                function f2f($num)
+                {
+                    $count = 0;
+                    for ($i = 1000; $i >= 50; $i = $i / 2) {
+                        $num = $i / 2;
+                        $count++;
+                    }
+                    echo 'Получится число:' . $num . '<br>Количество итераций:' . $count;
                 }
-                echo 'Получится число:'. $num .'<br>Количество итераций:'. $count;
+                f2f($num);
             ?>
     </fieldset>
     <fieldset>
@@ -56,12 +67,16 @@
             3 задание
         </legend>
             <?
-                for ($Si = 0; $Si <= 10; $Si++){
-                    echo '<br>'. "Si = $Si, то ";
-                    for ($j = 0; $j <= 10 - $Si; $j++){
-                        echo "$j ";
+                function f3()
+                {
+                    for ($Si = 0; $Si <= 10; $Si++)
+                    {
+                        echo '<br>' . "Si = $Si, то ";
+                        for ($j = 0; $j <= 10 - $Si; $j++)
+                            echo "$j ";
                     }
                 }
+                f3();
             ?>
     </fieldset>
     <fieldset>
@@ -70,7 +85,11 @@
         </legend>
         <?
             $str184 = 'html css php';
-            echo  str_split($str184,4)[0] ." ". substr($str184, 5, 3)." ". substr($str184, 9, 3);
+            function f184($str)
+            {
+                echo str_split($str, 4)[0] . " " . substr($str, 5, 3) . " " . substr($str, 9, 3);
+            }
+            f184($str184);
         ?>
     </fieldset>
     <fieldset>
@@ -79,9 +98,13 @@
         </legend>
         <?
             $str185 = '1.png';
-            $answer = '';
-            (substr($str185, -4) == '.png') ? $answer = 'да' : $answer = 'нет';
-            echo '1.png - ' . $answer;
+            function f185($str)
+            {
+                $answer = '';
+                (substr($str, -4) == '.png') ? $answer = 'да' : $answer = 'нет';
+                echo '1.png - ' . $answer;
+            }
+            f185($str185);
         ?>
     </fieldset>
     <fieldset>
@@ -90,11 +113,14 @@
         </legend>
         <?
             $str191 = 'qwertyuiop';
-            echo $str191;
-            if(strlen($str191) > 5){
-                $str191 = substr($str191,5) . '...';
+            function f191($str)
+            {
+                echo $str;
+                if (strlen($str) > 5)
+                    $str191 = substr($str, 5) . '...';
+                echo "<br>" . $str;
             }
-            echo "<br>" . $str191;
+            f191($str191);
         ?>
     </fieldset>
     <fieldset>
@@ -102,12 +128,16 @@
             Урок 7-8, слайд 19, задача 2
         </legend>
         <?
-        $str192 = 'abcstrabcstr';
-        echo $str192;
-        $str192 = str_replace('a' ,'1', $str192);
-        $str192 = str_replace('b' ,'2', $str192);
-        $str192 = str_replace('c' ,'3', $str192);
-        echo "<br>" . $str192;
+            $str192 = 'abcstrabcstr';
+            function f192($str)
+            {
+                echo $str;
+                $str192 = str_replace('a', '1', $str);
+                $str192 = str_replace('b', '2', $str);
+                $str192 = str_replace('c', '3', $str);
+                echo "<br>" . $str;
+            }
+            f192($str192);
         ?>
     </fieldset>
     <fieldset>
@@ -115,9 +145,13 @@
             Урок 7-8, слайд 19, задача 3
         </legend>
         <?
-        $str193 = 'abc abc abc';
-        echo $str193;
-        echo '<br> Позиция b - ' . strrpos($str193, 'b');
+            $str193 = 'abc abc abc';
+            function f193($str)
+            {
+                echo $str;
+                echo '<br> Позиция b - ' . strrpos($str, 'b');
+            }
+            f193($str193);
         ?>
     </fieldset>
     <fieldset>
@@ -125,12 +159,16 @@
             Урок 7-8, слайд 19, задача 4
         </legend>
         <?
-        $str194 = 'html css php';
-        $arrstr194=[];
-        echo $str194;
-        $arrstr194 = explode(' ', $str194);
-        echo '<br>';
-        print_r($arrstr194);
+            $str194 = 'html css php';
+            function f194($str)
+            {
+                $arrstr = [];
+                echo $str;
+                $arrstr = explode(' ', $str);
+                echo '<br>';
+                print_r($arrstr);
+            }
+            f194($str194);
         ?>
     </fieldset>
     <fieldset>
@@ -138,12 +176,110 @@
             Урок 7-8, слайд 19, задача 5
         </legend>
         <?
-        $str1951 = '12-02-2021';
-        $str1952 = '12-02-2022';
-        $count = 0;
-        echo 'Первая дата - ' . $str1951 . '<br> Вторая дата - ' . $str1952;
-        $count = (strtotime($str1952) - strtotime($str1951)) / (60 * 60 * 24);
-        echo '<br> Кол-во дней - ' . $count;
+            $str1951 = '12-02-2021';
+            $str1952 = '12-02-2022';
+            function f195($str1, $str2)
+            {
+                $count = 0;
+                echo 'Первая дата - ' . $str1 . '<br> Вторая дата - ' . $str2;
+                $count = (strtotime($str2) - strtotime($str1)) / (60 * 60 * 24);
+                echo '<br> Кол-во дней - ' . $count;
+            }
+            f195($str1951, $str1952);
+        ?>
+    </fieldset>
+    <fieldset>
+        <legend>
+            Урок 9-10, слайд 13, задача 7
+        </legend>
+        <?
+            function f7($x)
+            {
+                if ($x == 0)
+                    return 0;
+                if ($x == 1)
+                    return 1;
+                else
+                    return f7($x - 1) + f7($x - 2);
+            }
+            for ($i = 0; $i < 2; $i++)
+                echo f7($i) . ' ';
+        ?>
+    </fieldset>
+    <fieldset>
+        <legend>
+            Урок 9-10, слайд 14, задача 1
+        </legend>
+        <?
+        function f141($arr)
+        {
+            for ($i = 0;$i < count($arr); $i++)
+            {
+                $arr[$i] = rand(0,100);
+            }
+            return $arr;
+        }
+        $array = ['3','5','6'];
+        print_r(f141($array));
+        ?>
+    </fieldset>
+    <fieldset>
+        <legend>
+            Урок 9-10, слайд 14, задача 2
+        </legend>
+        <?
+            function f142($str)
+            {
+                return str_word_count(iconv("UTF-8", "windows-1251", $str));
+            }
+            $str142 = 'HTML, CSS, PHP, BITRIX';
+            echo f142($str142);
+        ?>
+    </fieldset>
+    <fieldset>
+        <legend>
+            Урок 9-10, слайд 14, задача 3
+        </legend>
+        <?
+        function f143($str)
+        {
+            $newstr = "";
+            for ($i = strlen($str); $i >= 0; $i--)
+            {
+                $newstr .= substr($str, $i,1);
+            }
+            return $newstr;
+        }
+        $str143 = 'HTML, CSS, PHP, BITRIX';
+        echo f143($str143);
+        ?>
+    </fieldset>
+    <fieldset>
+        <legend>
+            Урок 9-10, слайд 14, задача 4
+        </legend>
+        <?
+        function f144($str)
+        {
+            return strlen($str);
+        }
+        $str144 = 'HTML, CSS, PHP, BITRIX';
+        echo f144($str144);
+        ?>
+    </fieldset>
+    <fieldset>
+        <legend>
+            Урок 9-10, слайд 14, задача 5
+        </legend>
+        <?
+        function f145($str)
+        {
+            $str = preg_replace("|[^\d\w][\s]+|i","",$str);
+            for ($i = 0; $i < strlen($str); $i++)
+                    echo '<br>' . $str[$i];
+        }
+        $str145 = 'HTML, CSS, PHP, BITRIX';
+        f145($str145);
         ?>
     </fieldset>
 <!--Подключение footer-->
--- site/index.php
+++ site/index.php
@@ -1,34 +1,74 @@
 <!--Определение стиля в зависимости от времени-->
 <?
-    date_default_timezone_set('Asia/Yekaterinburg');
-    $time = date('H');
-    (($time >= 20 && $time < 24) || ($time >= 00 && $time < 8)) ?
-        $href="css/style-night.css" : $href="css/style.css";
-
+    function style_time()
+    {
+        date_default_timezone_set('Asia/Yekaterinburg');
+        $time = date('H');
+        (($time >= 20 && $time < 24) || ($time >= 00 && $time < 8)) ?
+            $href = "css/style-night.css" : $href = "css/style.css";
+        return $href;
+    }
 ?>
-<!--Функция для подсчета гласных в строке-->
+<!--Функция для подсчета гласных на странице-->
 <?
     $strings = [];
-    $vowels = 0;
-    $words = 0;
-    function vowels($str) {
-        $dictionary = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
-        $number = 0;
-        foreach ($dictionary as $value){
-            $number += substr_count($str, $value);
-            $number += substr_count($str, mb_strtoupper($value));
-        }
-        return $number;
-    }
     $strings[17] = 'Количество гласных букв';
     $strings[18] = 'Количество слов';
+    function vowels($strings) {
+        $dictionary = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
+        foreach ($strings as $value1)
+            foreach ($dictionary as $value2)
+            {
+                $number += substr_count($value1, $value2);
+                $number += substr_count($value1, mb_strtoupper($value2));
+            }
+        return $number;
+    }
 ?>
 <!--Количество прожитых дней-->
 <?
-    $birthday = '20-05-1999';
-    $date = date("m-d-Y");
-    $count = 0;
-    $count = intval((strtotime($date) - strtotime($birthday)) / (60 * 60 * 24));
+    function number_day()
+    {
+        $birthday = '20-05-1999';
+        $date = date("m-d-Y");
+        $count = intval((strtotime($date) - strtotime($birthday)) / (60 * 60 * 24));
+        return $count;
+    }
+?>
+<!--Функция вывода информации о себе-->
+<?
+    function about_me($str)
+    {
+        $arr_just_text1 = explode(':', $str);
+        echo "<font color='red'> $arr_just_text1[0]:</font>";
+        for ($i = 1; $i < count($arr_just_text1); $i++)
+            echo $arr_just_text1[$i];
+}
+?>
+<!--Функция вывода информации о курсах-->
+<?
+    function about_course($str)
+    {
+        $arr_just_text2 = explode(' ', $str);
+        $arr_i = 0;
+        foreach ($arr_just_text2 as $value)
+        {
+            if ($arr_i % 2 == 0)
+                echo "<font color='#008b8b'> $value </font>";
+            else
+                echo "<font color='#8b008b'> $value </font>";
+            $arr_i++;
+        }
+    }
+?>
+<!--Функция для подсчета слов на странице-->
+<?
+    function words($strings)
+    {
+        foreach ($strings as $value)
+            $words += str_word_count(iconv("UTF-8", "windows-1251", $value));
+        return $words;
+    }
 ?>
 <!doctype html>
 <html lang="ru">
@@ -38,7 +78,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Homework</title>
-    <link rel="stylesheet" type="text/css" href=<? echo $href; ?>>
+    <link rel="stylesheet" type="text/css" href=<? echo style_time() ?>>
 </head>
 <body class="body">
 
@@ -55,33 +95,20 @@
             <p class="just-text">
                 <?
                     $strings[2] = 'Место учёбы: МГТУ им. Г.И. Носова, 5 курс';
-                    $arr_just_text1 = explode(':', $strings[2]);
-                    echo "<font color='red'> $arr_just_text1[0]:</font>";
-                    for ($i = 1; $i < count($arr_just_text1); $i++) {
-                        echo $arr_just_text1[$i];
-                    }
+                    about_me($strings[2]);
                 ?>
             </p>
             <p class="just-text"><? echo $strings[3] = 'Специальность: Информационная безопасность автоматизированных систем'; ?></p>
             <p class="just-text"><? echo $strings[4] = 'Увлечения: велосипед, отдых на природе (походы), фотография, горные лыжи'; ?></p>
             <p class="just-text"><? echo $strings[5] = 'Дата рождения: 20.05.1999.'; ?></p>
-            <p class="just-text"><? $strings[6] = 'Прожитых дней:'; echo $strings[6] . " $count." ?></p>
+            <p class="just-text"><? $strings[6] = 'Прожитых дней:'; echo $strings[6] . number_day() . '.' ?></p>
         </div>
         <div class="about-course">
             <p class="topic"><? echo $strings[7] = 'О курсах'; ?></p>
             <p class="just-text">
                 <?
                     $strings[8] = 'Курсы идут хорошо, практика довольно полезна. Хочется больше ссылок на полезные дополнительные материалы по изучаемым темам.';
-                    $arr_just_text2 = explode(' ', $strings[8]);
-                    $arr_i = 0;
-                    foreach ($arr_just_text2 as $value) {
-                        if ($arr_i % 2 == 0){
-                            echo "<font color='#008b8b'> $value </font>";
-                        } else {
-                            echo "<font color='#8b008b'> $value </font>";
-                        }
-                        $arr_i++;
-                    }
+                    about_course($strings[8]);
                 ?>
             </p>
         </div>
@@ -123,14 +150,8 @@
         </section>
     </section>
     <section class="results">
-        <?
-            foreach ($strings as $value){
-                $vowels += vowels($value);
-                $words += str_word_count(iconv("UTF-8", "windows-1251", $value));
-            }
-        ?>
-        <div class="vowels"> <? echo "$strings[17] - $vowels."; ?> </div>
-        <div class="word"> <? echo "$strings[18] - $words."; ?> </div>
+        <div class="vowels"> <? echo "$strings[17] - " . vowels($strings) . '.'; ?> </div>
+        <div class="word"> <? echo "$strings[18] - " . words($strings) . '.' ; ?> </div>
     </section>
 
 <!--Подключение footer-->
